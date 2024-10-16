8bbb50cf2c2dab0409027c1f3d25aa44c89f371c
Gusti Rama Henry <g.ramahenry2439@gmail.com>
2024-10-16 12:58:27 -0400

    init 2

---
+++ main.cpp
@@ -0,0 +1,18 @@
+//
+//
+// bball_sim/
+// main.cpp
+//
+// Written by Gusti Rama Henry on 10/16/2024
+// Copyright (c) 2024 Gusti Rama Henry. All Rights Reserved
+//
+//
+
+
+#include <iostream>
+
+
+int main(){
+
+  return 0;
+}
